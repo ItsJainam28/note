b1849b7b5e68664b6771df85421f4820ae0bef11
Jainam Patel <jainampatel68@gmail.com>
2023-12-09 18:52:39 -0500

    Still fixing the Hosting issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
-
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+if(process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
